7c31dfbce2734eeb9ba49a979fed82910fa0af29
hilmytsaqif <hilmyfirdaus87@gmail.com>
2026-02-11 17:40:04 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'sertifikat.html'],
   theme: {
     container: {
       center: true,
